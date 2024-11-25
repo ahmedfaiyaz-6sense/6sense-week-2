c4ad028bf2abe6e6fd376d5811609704bf84546e
fuzzy <ahmed.faiyaz1532@gmail.com>
2024-11-25 13:32:49 +0600

    DTO applied

--- nestjs-task-management/src/tasks/tasks.controller.ts
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { CreateTaskDTO } from './dto/create-task.dto';
 //import { Task } from './tasks.model';
 @Controller('tasks')
 export class TasksController {
@@ -10,11 +11,15 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
-  @Post()
+  /*@Post()
   createTask(
     @Body('title') title: string,
     @Body('description') description: string,
   ) {
     return this.tasksService.createTask(title, description);
+  }*/
+  @Post()
+  createTask(@Body() createTaskDTO: CreateTaskDTO) {
+    return this.tasksService.createTask(createTaskDTO);
   }
 }
