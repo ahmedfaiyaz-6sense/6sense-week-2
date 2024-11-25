aed4d53dbb039632d8eb91125f7182669a63b48e
fuzzy <188864756+ahmedfaiyaz-6sense@users.noreply.github.com>
2024-11-25 13:13:38 +0600

    added create task

--- nestjs-task-management/src/tasks/tasks.service.ts
+++ nestjs-task-management/src/tasks/tasks.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './tasks.model';
+import { Task, TaskStatus } from './tasks.model';
+import { v6 as uuid } from 'uuid';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
   public getAllTasks() {
     return this.tasks;
+  }
+
+  public createTask(title: string, description: string) {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task
   }
 }
