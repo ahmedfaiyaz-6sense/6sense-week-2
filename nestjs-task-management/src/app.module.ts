7d384da9332d388975b5844127273d9befdfd679
fuzzy <188864756+ahmedfaiyaz-6sense@users.noreply.github.com>
2024-11-26 09:53:21 +0600

    added database

--- nestjs-task-management/src/app.module.ts
+++ nestjs-task-management/src/app.module.ts
@@ -1,6 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
+//console.log(process.env.host);
+
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    /*TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: String(process.env.host),
+      port: Number(process.env.port),
+      username: process.env.username,
+      password: process.env.password,
+      database: process.env.database,
+      synchronize: false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),*/
+  ],
 })
 export class AppModule {}
